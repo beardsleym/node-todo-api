84fbe4c3711b02adf1780e88bb70ef16b7c8d66f
Matt <matthew@beardsley.com.au>
2018-04-07 13:46:02 +0000

    removed require /secret mongolab key

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,10 @@
-const mongoLab = require('../../../../secret/mongoLab.js')
+// const mongoLab = require('../../../../secret/mongoLab.js')
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const mlabkey = process.env.MLABKEY || mongoLab.mLab
+// const mlabkey = process.env.MLABKEY || mongoLab.mLab
+const mlabkey = process.env.MLABKEY
 mongoose.connect(`mongodb://${mlabkey}.mlab.com:27199/todoapp`);
 
 module.exports = {mongoose};
