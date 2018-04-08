3fea239e2f51bcef2ff89b1b0dd69fa132a5015c
Matt <matthew@beardsley.com.au>
2018-04-08 03:17:00 +0000

    make require mlab key statement as part of if statement

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // const mlabkey = process.env.MLABKEY || mongoLab.mLab
-const mlabkey = process.env.MLABKEY
+const mlabkey = process.env.MLABKEY || require('../../../../secret/mongoLab.js').mLab
 mongoose.connect(`mongodb://${mlabkey}.mlab.com:27199/todoapp`);
 
 module.exports = {mongoose};
