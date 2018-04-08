f992d41ba36175d68f739dc6590b52c32f91b530
Matt <matthew@beardsley.com.au>
2018-04-08 04:00:09 +0000

    configured seperate mlab databases for c9 and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,12 @@
+// Made require statement as part of if statement as can't load /secret on heroku
 // const mongoLab = require('../../../../secret/mongoLab.js')
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// const mlabkey = process.env.MLABKEY || mongoLab.mLab
-const mlabkey = process.env.MLABKEY || require('../../../../secret/mongoLab.js').mLab
-mongoose.connect(`mongodb://${mlabkey}.mlab.com:27199/todoapp`);
+const mlabkey = process.env.MLABKEY || require('../../../../secret/mongoLab.js').mLabLocal
+
+mongoose.connect(`mongodb://${mlabkey}`);
 
 module.exports = {mongoose};
 
