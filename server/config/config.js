a6a9cf50282d55194dc980781cbbd47938bdc12f
Matt <matthew@beardsley.com.au>
2018-09-17 08:52:38 +0000

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,19 @@
 var env = process.env.NODE_ENV || 'development'
 console.log("Env ****", env)
 
-if(env === 'development') {
-  process.env.MLABKEY = require('../../../../secret/mongoLab.js').mLabLocal
-} else if (env === 'test') {
-  process.env.MLABKEY = require('../../../../secret/mongoLab.js').mLabTest
+if(env === 'development' || env === 'test') {
+  var config = require('../../../../secret/config.json');
+  var envConfig = config[env];
+  
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if(env === 'development') {
+//   process.env.MLABKEY = require('../../../../secret/mongoLab.js').mLabLocal
+// } else if (env === 'test') {
+//   process.env.MLABKEY = require('../../../../secret/mongoLab.js').mLabTest
+// }
 
 console.log("MongoDB ****", process.env.MLABKEY)
